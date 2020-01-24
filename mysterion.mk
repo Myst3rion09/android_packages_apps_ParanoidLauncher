3bfa97d615c07f4227fa6d7ae6fcb98576cb299c
Julian Fischer <myst3rion9@gmail.com>
2020-01-24 23:39:46 +0100

    mysterion: initial commit

---
+++ mysterion.mk
@@ -0,0 +1,9 @@
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/secure/releasekey
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/mysterion/overlay/lawnchair
+PRODUCT_COPY_FILES += \
+    vendor/mysterion/prebuilt/common/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \
+    vendor/mysterion/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
+
+PRODUCT_PACKAGES += \
+    Lawnchair
